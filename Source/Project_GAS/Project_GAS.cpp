346006e63316237b84d837921174ab7d45d26d26
Lichun <lcbiotech@126.com>
2024-03-21 07:01:19 +0800

    add files

---
+++ Source/Project_GAS/Project_GAS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_GAS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_GAS, "Project_GAS" );
